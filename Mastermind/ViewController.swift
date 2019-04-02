ed2913fe7f1a859f445c0023c3e9998bc1d65b2a
Sergiu BOCANCEA <sbocancea001@mac-salle253-11.iut.u-bordeaux-montaigne.fr>
2019-04-02 14:38:13 +0200

    Initial Commit

---
+++ Mastermind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mastermind
+//
+//  Created by Sergiu BOCANCEA on 02/04/2019.
+//  Copyright © 2019 Sergiu BOCANCEA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
